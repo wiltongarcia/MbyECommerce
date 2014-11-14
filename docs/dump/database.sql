bbd6ec4d26b7f1459a22d528708f9a92a8ffdb1d
Wilton de Oliveira Garcia <wiltonog@gmail.com>
2014-11-14 16:22:33 -0200

    alterei o dump para corrigir os campos que estavam aceitando valores NULL

--- docs/dump/database.sql
+++ docs/dump/database.sql
@@ -1,90 +1,151 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE categories
-(
-id int(11) NOT NULL AUTO_INCREMENT,
-title VARCHAR(255) UNIQUE,
-description TEXT,
-status tinyint(4) NOT NULL DEFAULT 1,
-updated_at TIMESTAMP  ON UPDATE CURRENT_TIMESTAMP,
-created_at TIMESTAMP NOT NULL,
-PRIMARY KEY (id)
-);
 
-CREATE TABLE characteristics
-(
-id int(11) NOT NULL AUTO_INCREMENT,
-title VARCHAR(255) UNIQUE,
-description TEXT,
-status tinyint(4) NOT NULL DEFAULT 1,
-updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-created_at TIMESTAMP NOT NULL,
-PRIMARY KEY (id)
-);
+# Dump of table categories
+# ------------------------------------------------------------
 
-CREATE TABLE orders
-(
-id int(11) NOT NULL AUTO_INCREMENT,
-updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-created_at TIMESTAMP NOT NULL,
-PRIMARY KEY (id)
-);
+DROP TABLE IF EXISTS `categories`;
 
-CREATE TABLE products
-(
-id int(11) NOT NULL AUTO_INCREMENT,
-title VARCHAR(255) UNIQUE,
-description TEXT,
-image VARCHAR(255) NOT NULL,
-price DECIMAL(10,2),
-status tinyint(4) NOT NULL DEFAULT 1,
-updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-created_at TIMESTAMP NOT NULL,
-PRIMARY KEY (id)
-);
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `description` text,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `title` (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE order_items
-(
-id int(11) NOT NULL AUTO_INCREMENT,
-order_id int(11),
-product_id int(11) NOT NULL,
-price DECIMAL(10,2) NOT NULL,
-quantity INTEGER NOT NULL,
-total DECIMAL(10,2) NOT NULL,
-status tinyint(4) NOT NULL DEFAULT 1,
-updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-created_at TIMESTAMP NOT NULL,
-PRIMARY KEY (id)
-);
 
-CREATE TABLE product_categories
-(
-id int(11) NOT NULL AUTO_INCREMENT,
-product_id int(11) NOT NULL,
-category_id int(11) NOT NULL,
-updated_at TIMESTAMP  ON UPDATE CURRENT_TIMESTAMP,
-created_at TIMESTAMP NOT NULL,
-PRIMARY KEY (id)
-);
 
-CREATE TABLE product_characteristics
-(
-id int(11) NOT NULL AUTO_INCREMENT,
-product_id int(11) NOT NULL,
-characteristic_id int(11) NOT NULL,
-updated_at TIMESTAMP  ON UPDATE CURRENT_TIMESTAMP,
-created_at TIMESTAMP NOT NULL,
-PRIMARY KEY (id)
-);
+# Dump of table characteristics
+# ------------------------------------------------------------
 
-ALTER TABLE order_items ADD FOREIGN KEY order_id_idxfk (order_id) REFERENCES orders (id);
+DROP TABLE IF EXISTS `characteristics`;
 
-ALTER TABLE order_items ADD FOREIGN KEY product_id_idxfk (product_id) REFERENCES products (id);
+CREATE TABLE `characteristics` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `description` text,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `title` (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE product_categories ADD FOREIGN KEY product_id_idxfk_1 (product_id) REFERENCES products (id);
 
-ALTER TABLE product_categories ADD FOREIGN KEY category_id_idxfk (category_id) REFERENCES categories (id);
 
-ALTER TABLE product_characteristics ADD FOREIGN KEY product_id_idxfk_2 (product_id) REFERENCES products (id);
+# Dump of table order_items
+# ------------------------------------------------------------
 
-ALTER TABLE product_characteristics ADD FOREIGN KEY characteristic_id_idxfk (characteristic_id) REFERENCES characteristics (id);
+DROP TABLE IF EXISTS `order_items`;
 
+CREATE TABLE `order_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `order_id_idxfk` (`order_id`),
+  KEY `product_id_idxfk` (`product_id`),
+  CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  CONSTRAINT `order_items_ibfk_3` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table orders
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table product_categories
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `product_categories`;
+
+CREATE TABLE `product_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `product_id_idxfk_1` (`product_id`),
+  KEY `category_id_idxfk` (`category_id`),
+  CONSTRAINT `product_categories_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+  CONSTRAINT `product_categories_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table product_characteristics
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `product_characteristics`;
+
+CREATE TABLE `product_characteristics` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `characteristic_id` int(11) NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `product_id_idxfk_2` (`product_id`),
+  KEY `characteristic_id_idxfk` (`characteristic_id`),
+  CONSTRAINT `product_characteristics_ibfk_2` FOREIGN KEY (`characteristic_id`) REFERENCES `characteristics` (`id`),
+  CONSTRAINT `product_characteristics_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table products
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `description` text NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `title` (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
