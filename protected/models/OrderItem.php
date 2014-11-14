0f6385af31f452301a36ba1dc8694372c0033c43
Wilton de Oliveira Garcia <wiltonog@gmail.com>
2014-11-14 15:41:37 -0200

    gerei o model da tabela order_items

---
+++ protected/models/OrderItem.php
@@ -0,0 +1,123 @@
+<?php
+
+/**
+ * This is the model class for table "order_items".
+ *
+ * The followings are the available columns in table 'order_items':
+ * @property integer $id
+ * @property integer $order_id
+ * @property integer $product_id
+ * @property string $price
+ * @property integer $quantity
+ * @property string $total
+ * @property integer $status
+ * @property string $updated_at
+ * @property string $created_at
+ *
+ * The followings are the available model relations:
+ * @property Products $product
+ * @property Orders $order
+ */
+class OrderItem extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'order_items';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('product_id, price, quantity, total', 'required'),
+			array('order_id, product_id, quantity, status', 'numerical', 'integerOnly'=>true),
+			array('price, total', 'length', 'max'=>10),
+			array('updated_at, created_at', 'safe'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, order_id, product_id, price, quantity, total, status, updated_at, created_at', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'product' => array(self::BELONGS_TO, 'Products', 'product_id'),
+			'order' => array(self::BELONGS_TO, 'Orders', 'order_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'order_id' => 'Order',
+			'product_id' => 'Product',
+			'price' => 'Price',
+			'quantity' => 'Quantity',
+			'total' => 'Total',
+			'status' => 'Status',
+			'updated_at' => 'Updated At',
+			'created_at' => 'Created At',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('order_id',$this->order_id);
+		$criteria->compare('product_id',$this->product_id);
+		$criteria->compare('price',$this->price,true);
+		$criteria->compare('quantity',$this->quantity);
+		$criteria->compare('total',$this->total,true);
+		$criteria->compare('status',$this->status);
+		$criteria->compare('updated_at',$this->updated_at,true);
+		$criteria->compare('created_at',$this->created_at,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return OrderItem the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
