a1123cda1a2f1928548a4cce386639785c1264ec
Wilton de Oliveira Garcia <wiltonog@gmail.com>
2014-11-14 21:47:32 -0200

    alterei a rota do link da listagem para update

--- protected/views/categories/_view.php
+++ protected/views/categories/_view.php
@@ -6,7 +6,7 @@
 <div class="view">
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->title), array('view', 'id'=>$data->id)); ?>
+	<?php echo CHtml::link(CHtml::encode($data->title), array('update', 'id'=>$data->id)); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('description')); ?>:</b>
