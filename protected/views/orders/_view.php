29244fcdc649ab4949a0394bdf4bb87855d54b86
Wilton de Oliveira Garcia <wiltonog@gmail.com>
2014-11-14 21:00:38 -0200

    gerei o CRUD de orders

---
+++ protected/views/orders/_view.php
@@ -0,0 +1,21 @@
+<?php
+/* @var $this OrdersController */
+/* @var $data Order */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('updated_at')); ?>:</b>
+	<?php echo CHtml::encode($data->updated_at); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('created_at')); ?>:</b>
+	<?php echo CHtml::encode($data->created_at); ?>
+	<br />
+
+
+</div>
---
+++ protected/views/orders/create.php
@@ -0,0 +1,18 @@
+<?php
+/* @var $this OrdersController */
+/* @var $model Order */
+
+$this->breadcrumbs=array(
+	'Orders'=>array('index'),
+	'Create',
+);
+
+$this->menu=array(
+	array('label'=>'List Order', 'url'=>array('index')),
+	array('label'=>'Manage Order', 'url'=>array('admin')),
+);
+?>
+
+<h1>Create Order</h1>
+
+<?php $this->renderPartial('_form', array('model'=>$model)); ?>
