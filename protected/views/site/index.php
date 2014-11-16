ea1a13c3171c520b01425ad69f9fc6b57406e550
Wilton de Oliveira Garcia <wiltonog@gmail.com>
2014-11-16 09:55:39 -0200

    integrei o sidebar com os outros templates

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,21 +1,6 @@
-            <div id="column-right">
-                <div class="box">
-                    <div class="box-heading">Categories</div>
-                    <div class="box-content">
-                        <div class="box-category">
-                            <ul>
-                                <?php foreach ($categories as $k=>$c): $c=(object)$c; ?>
-                                <li>
-                                    <a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=product/category&amp;path=71"><?php echo $c->title; ?> (<?php echo $c->products_total; ?>)</a>
-                                </li>
-                                <?php endForeach; ?>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php $this->renderPartial('_sidebar', array('categories'=>$categories)); ?>
             <div id="content">
                 <?php $this->renderPartial('_list', array('list'=>$latest, 'name'=>'Latest')); ?>
-                </div>
                 <h1 style="display: none;">Your Store</h1>
             </div>
+            <?php $this->renderPartial('_scriptcart'); ?>
