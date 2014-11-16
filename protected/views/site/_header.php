06a1538b77429f318485cca0e7ac79e7c2b4a7c3
Wilton de Oliveira Garcia <wiltonog@gmail.com>
2014-11-16 09:54:31 -0200

    integrei o header

--- protected/views/site/_header.php
+++ protected/views/site/_header.php
@@ -3,22 +3,22 @@
                 <div id="header">
                     <!-- Logo Starts -->
                     <div id="logo">
-                        <a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=common/home">
-                            <img src="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/image/data/logo.png" title="Mystockimages" alt="Mystockimages" />
+                        <a href="<?php echo Yii::app()->createUrl('site/index'); ?>">
+                            <img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/site/images/logo.png" title="Mystockimages" alt="Mystockimages" />
                         </a>
                     </div>
                     <!-- Logo Ends -->
-                    <!-- Languages Starts -->
-                    <!-- Languages Ends -->
                     <!-- Search Starts -->
                     <div id="search">
-                        <div class="button-search"></div>
-                        <input type="text" name="filter_name" value="Search" onclick="this.value = '';" onkeydown="this.style.color = '#b1b1b1';" />
+                        <form action="<?php echo Yii::app()->createUrl('site/search'); ?>" method="get" accept-charset="utf-8">
+                            <button type="submit" style="border:none" class="button-search"></button>
+                            <input type="text" name="q" value="Search" onclick="this.value = '';" onkeydown="this.style.color = '#b1b1b1';" />
+                        </form>
                     </div>
                     <!-- Search Ends -->
                     <!-- Welcome Text Starts -->
                     <div id="welcome">
-                        Welcome visitor you can <a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=account/login">login</a> or <a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=account/register">create an account</a>.									
+                        Welcome visitor you can <a href="<?php echo Yii::app()->createUrl('site/login'); ?>">login</a>.									
                     </div>
                     <!-- Welcome Text Ends -->
                     <!-- Header Links Starts -->
@@ -26,7 +26,7 @@
                         <!-- Cart Starts -->
                         <div id="cart">
                             <div class="heading clearfix">
-                                <a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=checkout/cart">
+                                <a href="<?php echo Yii::app()->createUrl('site/cart'); ?>">
                                 Shopping Cart							
                                 </a>&nbsp;
                                 <span id="cart_total">0 item(s) - $0.00</span>							
@@ -38,17 +38,10 @@
                         <div class="links clearfix">
                             <ul>
                                 <li>
-                                    <a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=common/home">Home</a>
+                                    <a href="<?php echo Yii::app()->createUrl('site/index'); ?>">Home</a>
                                 </li>
                                 <li>|</li>
-                                <li>
-                                    <a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=account/wishlist" id="wishlist_total">
-                                    Wish List (0)								</a>
-                                </li>
-                                <li>|</li>
-                                <li><a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=account/account">My Account</a></li>
-                                <li>|</li>
-                                <li><a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=checkout/checkout">Checkout</a></li>
+                                <li><a href="<?php echo Yii::app()->createUrl('site/login'); ?>">My Account</a></li>
                                 <li>|</li>
                             </ul>
                         </div>
@@ -57,62 +50,4 @@
                     <!-- Header Links Ends -->
                 </div>
                 <!-- Header Ends -->	
-                <!-- Main Menu Starts -->
-                <div id="menu">
-                    <ul>
-                        <!-- Menu Links Starts -->
-                        <li>
-                            <a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=product/category&amp;path=59">Chilly</a>
-                            <!-- Dropdown Menu Links Starts -->
-                            <div>
-                                <ul>
-                                    <li>
-                                        <a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=product/category&amp;path=59_66">
-                                        Indian (1)															</a>
-                                    </li>
-                                    <li>
-                                        <a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=product/category&amp;path=59_65">
-                                        Mexican (0)															</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <!-- Dropdown Menu Links Ends -->
-                        </li>
-                        <li><a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=product/category&amp;path=60">Morning Saga</a>
-                        </li>
-                        <li><a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=product/category&amp;path=61">Nature</a>
-                        </li>
-                        <li><a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=product/category&amp;path=62">Stationary</a>
-                        </li>
-                        <li><a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=product/category&amp;path=63">Textures</a>
-                        </li>
-                        <li>
-                            <a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=product/category&amp;path=64">Toys</a>
-                            <!-- Dropdown Menu Links Starts -->
-                            <div>
-                                <ul>
-                                    <li>
-                                        <a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=product/category&amp;path=64_67">
-                                        0 - 1 yrs (0)															</a>
-                                    </li>
-                                    <li>
-                                        <a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=product/category&amp;path=64_68">
-                                        1 - 2 yrs (1)															</a>
-                                    </li>
-                                    <li>
-                                        <a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=product/category&amp;path=64_69">
-                                        2 - 4 yrs (0)															</a>
-                                    </li>
-                                    <li>
-                                        <a href="http://santoshsetty.com/tf/opencart-templates/mystockimageshop-v15/index.php?route=product/category&amp;path=64_70">
-                                        5 - 8 yrs (0)															</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <!-- Dropdown Menu Links Ends -->
-                        </li>
-                        <!-- Menu Links Ends -->
-                    </ul>
-                </div>
-                <!-- Main Menu Ends -->
             </div>
