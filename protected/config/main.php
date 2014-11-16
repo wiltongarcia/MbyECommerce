b5f7d4fa8ff874f5f7b0e9c0ec3414f75f757d4e
Wilton de Oliveira Garcia <wiltonog@gmail.com>
2014-11-16 17:20:55 -0200

    desabilitei o gii

--- protected/config/main.php
+++ protected/config/main.php
@@ -15,17 +15,17 @@ return array(
 	// autoloading model and component classes
 	'import'=>array(
 		'application.models.*',
-		'application.components.*',
+        'application.components.*',
 	),
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		'gii'=>array(
+		/*'gii'=>array(
 			'class'=>'system.gii.GiiModule',
 			'password'=>'123456',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
-		),
+		),*/
 	),
 
 	// application components
